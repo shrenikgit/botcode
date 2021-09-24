300b87826aa5586ee1b5631da2a888182a6b2782
shrenikgit <91279080+shrenikgit@users.noreply.github.com>
2021-09-24 10:49:42 +0530

    new apppy

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
-from flask import request
+from flask import Flask,request,Response
+from botbuilder.schema import Activity
+from botbuilder.core import BotFrameworkAdapter,BotFrameworkAdapterSettings
+import asyncio
+
+from echobot import EchoBot
+
 app = Flask(__name__)
+loop = asyncio.get_event_loop()
 
-@app.route('/hello/', methods=['GET'])
-def welcome():
-    return "Hello World!"
+botadaptersettings = BotFrameworkAdapterSettings("","")
+botadapter = BotFrameworkAdapter(botadaptersettings)
 
+ebot = EchoBot()
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=9876)
+@app.route("/api/messages",methods=["POST"])
+def messages():
+    if "application/json" in request.headers["content-type"]:
+      jsonmessage = request.json
+    else:
+      return Response(status=415)
+
+    activity = Activity().deserialize(jsonmessage)
+
+    async def turn_call(turn_context):
+        await ebot.on_turn(turn_context)
+
+    task = loop.create_task(botadapter.process_activity(activity,"",turn_call))
+    loop.run_until_complete(task)
+
+if __name__ == '__main__':
+    app.run('localhost',3978)
